7a072bd7b61001518704b0e63be2a912d497e3fa
srizmapaudel <paudelsrizmaa007@gmail.com>
2024-01-08 08:25:50 +0545

    js basics

---
+++ javascript.js
@@ -0,0 +1,37 @@
+const country="Nepal";//constant declaration using const
+let studentname='ram';//variable declaration using let
+var a="ram";//variable declarfation using var
+let fullInfo='${studentName} from ${country} .${new Date()}';
+let concatedName="Ram"+"Rai";
+console.log(concatedName);
+console.log(fullInfo);
+a='hari'
+
+
+
+//Number
+let num1=10;
+let num2=20;.2;
+let num3=-20.2;
+num1="changed"//example of loosely types language
+console.log(num1);
+
+
+
+
+//boolean
+let isNEPALI=true;
+let isChinese=false;
+let num4=10;
+let num4String="10";
+console.log(num4==num4String);//true::only value is checked
+console.log(num4===num4String);//false datatype is checked
+console.log(num4+num4String); //Resuly 1010
+console.log(num4-num4String); //result 0 -does arithmetic operation 
+console.log (typeof num4); //Number
+console.log (typeof num4String); //String
+console.log(typeof isChinese);//boolean
+
+
+
+
